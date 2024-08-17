e0eecd3cb00d38b398362630552c5a0034a2b4b0
Seu Nome <ismaelzmedeiros007@gmail.com>
2024-08-17 14:06:16 -0300

    17.08

--- PrimaryJS/scriptmenu.js
+++ PrimaryJS/scriptmenu.js
@@ -2,13 +2,42 @@ const typeNav = document.getElementById('typenav');
 const conteinerImgNav = document.getElementById('conteinerimgnav');
 
 conteinerImgNav.addEventListener('click', function() {
-    this.classList.toggle('conteinerimgnavclick');
-
+    //
     if (typeNav.style.display === 'flex') {
         typeNav.style.display = 'none';
     } else {
         typeNav.style.display = 'flex';
     }
+    //
+    if (typeNav.style.display === 'flex') {
+        conteinerImgNav.style.borderRadius = '50% 50% 0% 0%';
+    } else {
+        conteinerImgNav.style.borderRadius = '50%';
+    }
+    //
+    if (body.style.overflowY === 'hidden') {
+        body.style.overflowY = 'auto';
+    } else {
+        body.style.overflowY = 'hidden';
+    }
+});
+//Provavelmente tem um jeito melhor de fazer isso, mas foi o que eu aprendi ent boa sorte!
+const closeText = document.getElementById('typetextclose');
+
+closeText.addEventListener('click', function() {
+    //
+    if (typeNav.style.display === 'flex') {
+        typeNav.style.display = 'none';
+    } else {
+        typeNav.style.display = 'flex';
+    }
+    //
+    if (typeNav.style.display === 'flex') {
+        conteinerImgNav.style.borderRadius = '50% 50% 0% 0%';
+    } else {
+        conteinerImgNav.style.borderRadius = '50%';
+    }
+    //
     if (body.style.overflowY === 'hidden') {
         body.style.overflowY = 'auto';
     } else {
@@ -16,3 +45,439 @@ conteinerImgNav.addEventListener('click', function() {
     }
 });
 
+const fecharEmail = document.getElementById('typeemailclose');
+
+fecharEmail.addEventListener('click', function() {
+    //
+    if (typeNav.style.display === 'flex') {
+        typeNav.style.display = 'none';
+    } else {
+        typeNav.style.display = 'flex';
+    }
+    //
+    if (typeNav.style.display === 'flex') {
+        conteinerImgNav.style.borderRadius = '50% 50% 0% 0%';
+    } else {
+        conteinerImgNav.style.borderRadius = '50%';
+    }
+    //
+    if (body.style.overflowY === 'hidden') {
+        body.style.overflowY = 'auto';
+    } else {
+        body.style.overflowY = 'hidden';
+    }
+});
+
+const fecharPassword = document.getElementById('typepasswordclose');
+
+fecharEmail.addEventListener('click', function() {
+    //
+    if (typeNav.style.display === 'flex') {
+        typeNav.style.display = 'none';
+    } else {
+        typeNav.style.display = 'flex';
+    }
+    //
+    if (typeNav.style.display === 'flex') {
+        conteinerImgNav.style.borderRadius = '50% 50% 0% 0%';
+    } else {
+        conteinerImgNav.style.borderRadius = '50%';
+    }
+    //
+    if (body.style.overflowY === 'hidden') {
+        body.style.overflowY = 'auto';
+    } else {
+        body.style.overflowY = 'hidden';
+    }
+});
+
+const closeCheckbox = document.getElementById('typecheckboxclose');
+
+closeCheckbox.addEventListener('click', function() {
+    //
+    if (typeNav.style.display === 'flex') {
+        typeNav.style.display = 'none';
+    } else {
+        typeNav.style.display = 'flex';
+    }
+    //
+    if (typeNav.style.display === 'flex') {
+        conteinerImgNav.style.borderRadius = '50% 50% 0% 0%';
+    } else {
+        conteinerImgNav.style.borderRadius = '50%';
+    }
+    //
+    if (body.style.overflowY === 'hidden') {
+        body.style.overflowY = 'auto';
+    } else {
+        body.style.overflowY = 'hidden';
+    }
+});
+
+const closeDate = document.getElementById('typedateclose');
+
+closeDate.addEventListener('click', function() {
+    //
+    if (typeNav.style.display === 'flex') {
+        typeNav.style.display = 'none';
+    } else {
+        typeNav.style.display = 'flex';
+    }
+    //
+    if (typeNav.style.display === 'flex') {
+        conteinerImgNav.style.borderRadius = '50% 50% 0% 0%';
+    } else {
+        conteinerImgNav.style.borderRadius = '50%';
+    }
+    //
+    if (body.style.overflowY === 'hidden') {
+        body.style.overflowY = 'auto';
+    } else {
+        body.style.overflowY = 'hidden';
+    }
+});
+
+const closeColor = document.getElementById('typecolorclose');
+
+closeColor.addEventListener('click', function() {
+    //
+    if (typeNav.style.display === 'flex') {
+        typeNav.style.display = 'none';
+    } else {
+        typeNav.style.display = 'flex';
+    }
+    //
+    if (typeNav.style.display === 'flex') {
+        conteinerImgNav.style.borderRadius = '50% 50% 0% 0%';
+    } else {
+        conteinerImgNav.style.borderRadius = '50%';
+    }
+    //
+    if (body.style.overflowY === 'hidden') {
+        body.style.overflowY = 'auto';
+    } else {
+        body.style.overflowY = 'hidden';
+    }
+});
+
+const closeButton = document.getElementById('typebuttonclose');
+
+closeButton.addEventListener('click', function() {
+    //
+    if (typeNav.style.display === 'flex') {
+        typeNav.style.display = 'none';
+    } else {
+        typeNav.style.display = 'flex';
+    }
+    //
+    if (typeNav.style.display === 'flex') {
+        conteinerImgNav.style.borderRadius = '50% 50% 0% 0%';
+    } else {
+        conteinerImgNav.style.borderRadius = '50%';
+    }
+    //
+    if (body.style.overflowY === 'hidden') {
+        body.style.overflowY = 'auto';
+    } else {
+        body.style.overflowY = 'hidden';
+    }
+});
+
+const closeRange = document.getElementById('typeerangeclose');
+
+closeRange.addEventListener('click', function() {
+    //
+    if (typeNav.style.display === 'flex') {
+        typeNav.style.display = 'none';
+    } else {
+        typeNav.style.display = 'flex';
+    }
+    //
+    if (typeNav.style.display === 'flex') {
+        conteinerImgNav.style.borderRadius = '50% 50% 0% 0%';
+    } else {
+        conteinerImgNav.style.borderRadius = '50%';
+    }
+    //
+    if (body.style.overflowY === 'hidden') {
+        body.style.overflowY = 'auto';
+    } else {
+        body.style.overflowY = 'hidden';
+    }
+});
+
+const closeRadio = document.getElementById('typeradioclose');
+
+closeRadio.addEventListener('click', function() {
+    //
+    if (typeNav.style.display === 'flex') {
+        typeNav.style.display = 'none';
+    } else {
+        typeNav.style.display = 'flex';
+    }
+    //
+    if (typeNav.style.display === 'flex') {
+        conteinerImgNav.style.borderRadius = '50% 50% 0% 0%';
+    } else {
+        conteinerImgNav.style.borderRadius = '50%';
+    }
+    //
+    if (body.style.overflowY === 'hidden') {
+        body.style.overflowY = 'auto';
+    } else {
+        body.style.overflowY = 'hidden';
+    }
+});
+
+const closeUrl = document.getElementById('typeurlclose');
+
+closeUrl.addEventListener('click', function() {
+    //
+    if (typeNav.style.display === 'flex') {
+        typeNav.style.display = 'none';
+    } else {
+        typeNav.style.display = 'flex';
+    }
+    //
+    if (typeNav.style.display === 'flex') {
+        conteinerImgNav.style.borderRadius = '50% 50% 0% 0%';
+    } else {
+        conteinerImgNav.style.borderRadius = '50%';
+    }
+    //
+    if (body.style.overflowY === 'hidden') {
+        body.style.overflowY = 'auto';
+    } else {
+        body.style.overflowY = 'hidden';
+    }
+});
+
+const closeReset = document.getElementById('typeresetclose');
+
+closeReset.addEventListener('click', function() {
+    //
+    if (typeNav.style.display === 'flex') {
+        typeNav.style.display = 'none';
+    } else {
+        typeNav.style.display = 'flex';
+    }
+    //
+    if (typeNav.style.display === 'flex') {
+        conteinerImgNav.style.borderRadius = '50% 50% 0% 0%';
+    } else {
+        conteinerImgNav.style.borderRadius = '50%';
+    }
+    //
+    if (body.style.overflowY === 'hidden') {
+        body.style.overflowY = 'auto';
+    } else {
+        body.style.overflowY = 'hidden';
+    }
+});
+
+const closeDateTimeLocal = document.getElementById('typedatetime-localclose');
+
+closeDateTimeLocal.addEventListener('click', function() {
+    //
+    if (typeNav.style.display === 'flex') {
+        typeNav.style.display = 'none';
+    } else {
+        typeNav.style.display = 'flex';
+    }
+    //
+    if (typeNav.style.display === 'flex') {
+        conteinerImgNav.style.borderRadius = '50% 50% 0% 0%';
+    } else {
+        conteinerImgNav.style.borderRadius = '50%';
+    }
+    //
+    if (body.style.overflowY === 'hidden') {
+        body.style.overflowY = 'auto';
+    } else {
+        body.style.overflowY = 'hidden';
+    }
+});
+
+const closeMonth = document.getElementById('typemonthclose');
+
+closeMonth.addEventListener('click', function() {
+    //
+    if (typeNav.style.display === 'flex') {
+        typeNav.style.display = 'none';
+    } else {
+        typeNav.style.display = 'flex';
+    }
+    //
+    if (typeNav.style.display === 'flex') {
+        conteinerImgNav.style.borderRadius = '50% 50% 0% 0%';
+    } else {
+        conteinerImgNav.style.borderRadius = '50%';
+    }
+    //
+    if (body.style.overflowY === 'hidden') {
+        body.style.overflowY = 'auto';
+    } else {
+        body.style.overflowY = 'hidden';
+    }
+});
+
+const closeWeek = document.getElementById('typeweekclose');
+
+closeWeek.addEventListener('click', function() {
+    //
+    if (typeNav.style.display === 'flex') {
+        typeNav.style.display = 'none';
+    } else {
+        typeNav.style.display = 'flex';
+    }
+    //
+    if (typeNav.style.display === 'flex') {
+        conteinerImgNav.style.borderRadius = '50% 50% 0% 0%';
+    } else {
+        conteinerImgNav.style.borderRadius = '50%';
+    }
+    //
+    if (body.style.overflowY === 'hidden') {
+        body.style.overflowY = 'auto';
+    } else {
+        body.style.overflowY = 'hidden';
+    }
+});
+
+const closeTel = document.getElementById('typetelclose');
+
+closeTel.addEventListener('click', function() {
+    //
+    if (typeNav.style.display === 'flex') {
+        typeNav.style.display = 'none';
+    } else {
+        typeNav.style.display = 'flex';
+    }
+    //
+    if (typeNav.style.display === 'flex') {
+        conteinerImgNav.style.borderRadius = '50% 50% 0% 0%';
+    } else {
+        conteinerImgNav.style.borderRadius = '50%';
+    }
+    //
+    if (body.style.overflowY === 'hidden') {
+        body.style.overflowY = 'auto';
+    } else {
+        body.style.overflowY = 'hidden';
+    }
+});
+
+const closeFile = document.getElementById('typefileclose');
+
+closeFile.addEventListener('click', function() {
+    //
+    if (typeNav.style.display === 'flex') {
+        typeNav.style.display = 'none';
+    } else {
+        typeNav.style.display = 'flex';
+    }
+    //
+    if (typeNav.style.display === 'flex') {
+        conteinerImgNav.style.borderRadius = '50% 50% 0% 0%';
+    } else {
+        conteinerImgNav.style.borderRadius = '50%';
+    }
+    //
+    if (body.style.overflowY === 'hidden') {
+        body.style.overflowY = 'auto';
+    } else {
+        body.style.overflowY = 'hidden';
+    }
+});
+
+const closeHidden = document.getElementById('typehiddenclose');
+
+closeHidden.addEventListener('click', function() {
+    //
+    if (typeNav.style.display === 'flex') {
+        typeNav.style.display = 'none';
+    } else {
+        typeNav.style.display = 'flex';
+    }
+    //
+    if (typeNav.style.display === 'flex') {
+        conteinerImgNav.style.borderRadius = '50% 50% 0% 0%';
+    } else {
+        conteinerImgNav.style.borderRadius = '50%';
+    }
+    //
+    if (body.style.overflowY === 'hidden') {
+        body.style.overflowY = 'auto';
+    } else {
+        body.style.overflowY = 'hidden';
+    }
+});
+
+const closeImage = document.getElementById('typeimageclose');
+
+closeImage.addEventListener('click', function() {
+    //
+    if (typeNav.style.display === 'flex') {
+        typeNav.style.display = 'none';
+    } else {
+        typeNav.style.display = 'flex';
+    }
+    //
+    if (typeNav.style.display === 'flex') {
+        conteinerImgNav.style.borderRadius = '50% 50% 0% 0%';
+    } else {
+        conteinerImgNav.style.borderRadius = '50%';
+    }
+    //
+    if (body.style.overflowY === 'hidden') {
+        body.style.overflowY = 'auto';
+    } else {
+        body.style.overflowY = 'hidden';
+    }
+});
+
+const closeSearch = document.getElementById('typesearchclose');
+
+closeSearch.addEventListener('click', function() {
+    //
+    if (typeNav.style.display === 'flex') {
+        typeNav.style.display = 'none';
+    } else {
+        typeNav.style.display = 'flex';
+    }
+    //
+    if (typeNav.style.display === 'flex') {
+        conteinerImgNav.style.borderRadius = '50% 50% 0% 0%';
+    } else {
+        conteinerImgNav.style.borderRadius = '50%';
+    }
+    //
+    if (body.style.overflowY === 'hidden') {
+        body.style.overflowY = 'auto';
+    } else {
+        body.style.overflowY = 'hidden';
+    }
+});
+
+const closeNumber = document.getElementById('typenumberclose');
+
+closeNumber.addEventListener('click', function() {
+    //
+    if (typeNav.style.display === 'flex') {
+        typeNav.style.display = 'none';
+    } else {
+        typeNav.style.display = 'flex';
+    }
+    //
+    if (typeNav.style.display === 'flex') {
+        conteinerImgNav.style.borderRadius = '50% 50% 0% 0%';
+    } else {
+        conteinerImgNav.style.borderRadius = '50%';
+    }
+    //
+    if (body.style.overflowY === 'hidden') {
+        body.style.overflowY = 'auto';
+    } else {
+        body.style.overflowY = 'hidden';
+    }
+});
